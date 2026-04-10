75ee3b440f17b87137d3e65d6609f3cb8a1bb4df
MontecalvoAm <aljon.bajenting101@gmail.com>
2026-04-10 13:23:36 +0800

    chore: move obsolete debug/test/migration files to for-deletion/

--- test_date_local.js
+++
@@ -1,6 +0,0 @@
-const dateObj = new Date(2000, 0, 19); // Jan 19, 2000 Local
-console.log("Date Object:", dateObj);
-console.log("JSON Stringify:", JSON.stringify(dateObj));
-const jsonStr = JSON.stringify(dateObj).replace(/"/g, '');
-console.log("Substring(0,10):", jsonStr.substring(0, 10));
-console.log("toLocaleDateString(en-CA):", dateObj.toLocaleDateString('en-CA'));
